4a4e499ff69e58d667ae360ecfe59d05f128c037
Eike Kettner <eike.kettner@posteo.de>
2024-02-27 16:42:34 +0100

    Add few more tests

---
+++ modules/search-query/src/test/scala/io/renku/search/query/DateTimeRefSpec.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2024 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.search.query
+
+import cats.syntax.all.*
+import munit.FunSuite
+import java.time.Instant
+import java.time.ZoneId
+import java.time.Period
+
+class DateTimeRefSpec extends FunSuite:
+
+  val refDate: Instant = Instant.parse("2024-02-27T15:34:55Z")
+  val utc: ZoneId = ZoneId.of("UTC")
+
+  test("resolve relative dates") {
+    assertEquals(
+      RelativeDate.Today.resolve,
+      (refDate, None)
+    )
+    assertEquals(
+      DateTimeRef.Relative(RelativeDate.Yesterday).resolve(refDate, utc),
+      (refDate.atZone(utc).minusDays(1).toInstant(), None)
+    )
+  }
+
+  test("resolve partial date") {
+    val may = PartialDateTime.unsafeFromString("2023-05")
+    assertEquals(
+      may.resolve,
+      (may.instantMin(utc), may.instantMax(utc).some)
+    )
+    val exact = PartialDateTime.fromInstant(Instant.EPOCH)
+    assertEquals(exact.resolve, (exact.instantMin(utc), None))
+  }
+
+  test("resolve date calc") {
+    val may = PartialDateTime.unsafeFromString("2023-05")
+    val calc1 = DateTimeCalc(may, Period.ofDays(5), false)
+    assertEquals(
+      calc1.resolve,
+      (may.instantMin(utc).atZone(utc).plusDays(5).toInstant(), None)
+    )
+
+    val calc2 = DateTimeCalc(may, Period.ofDays(-5), false)
+    assertEquals(
+      calc2.resolve,
+      (may.instantMin(utc).atZone(utc).minusDays(5).toInstant(), None)
+    )
+
+    val range = DateTimeCalc(may, Period.ofDays(5), true)
+    assertEquals(
+      range.resolve,
+      (
+        may.instantMin(utc).atZone(utc).minusDays(5).toInstant(),
+        may.instantMin(utc).atZone(utc).plusDays(5).toInstant().some
+      )
+    )
+  }
+
+  extension (r: RelativeDate) def resolve = DateTimeRef(r).resolve(refDate, utc)
+  extension (d: PartialDateTime) def resolve = DateTimeRef(d).resolve(refDate, utc)
+  extension (r: DateTimeCalc) def resolve = DateTimeRef(r).resolve(refDate, utc)
---
+++ modules/search-solr-client/src/test/scala/io/renku/search/solr/query/LuceneQueryEncoderSpec.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2024 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.search.solr.query
+
+import munit.FunSuite
+import cats.Id
+import cats.data.NonEmptyList as Nel
+import io.renku.search.query.{Comparison, FieldTerm}
+import java.time.{Instant, ZoneId}
+import io.renku.search.query.*
+
+class LuceneQueryEncoderSpec extends FunSuite with LuceneQueryEncoders:
+
+  val refDate: Instant = Instant.parse("2024-02-27T15:34:55Z")
+  val utc: ZoneId = ZoneId.of("UTC")
+
+  val ctx: Context[Id] = Context.fixed(refDate, utc)
+  val createdEncoder = SolrTokenEncoder[Id, FieldTerm.Created]
+
+  test("use date-max for greater-than"):
+    val pd = PartialDateTime.unsafeFromString("2023-05")
+    val date: FieldTerm.Created =
+      FieldTerm.Created(Comparison.GreaterThan, Nel.of(DateTimeRef(pd)))
+    assertEquals(
+      createdEncoder.encode(ctx, date),
+      SolrQuery(
+        SolrToken.fromField(Field.Created) ~
+          SolrToken.fromComparison(Comparison.GreaterThan) ~
+          SolrToken.fromInstant(pd.instantMax(utc))
+      )
+    )
+
+  test("use date-min for lower-than"):
+    val pd = PartialDateTime.unsafeFromString("2023-05")
+    val date: FieldTerm.Created =
+      FieldTerm.Created(Comparison.LowerThan, Nel.of(DateTimeRef(pd)))
+    assertEquals(
+      createdEncoder.encode(ctx, date),
+      SolrQuery(
+        SolrToken.fromField(Field.Created) ~
+          SolrToken.fromComparison(Comparison.LowerThan) ~
+          SolrToken.fromInstant(pd.instantMin(utc))
+      )
+    )
+
+  test("created comparison is"):
+    val cToday: FieldTerm.Created =
+      FieldTerm.Created(Comparison.Is, Nel.of(DateTimeRef(RelativeDate.Today)))
+    assertEquals(
+      createdEncoder.encode(ctx, cToday),
+      SolrQuery(SolrToken.dateIs(Field.Created, refDate))
+    )
+
+  test("single range"):
+    val pd = PartialDateTime.unsafeFromString("2023-05")
+    val date: FieldTerm.Created =
+      FieldTerm.Created(Comparison.Is, Nel.of(DateTimeRef(pd)))
+    assertEquals(
+      createdEncoder.encode(ctx, date),
+      SolrQuery(
+        SolrToken.fieldIs(
+          Field.Created,
+          SolrToken.fromDateRange(pd.instantMin(utc), pd.instantMax(utc))
+        )
+      )
+    )
+
+  test("multiple range"):
+    val pd1 = PartialDateTime.unsafeFromString("2023-05")
+    val pd2 = PartialDateTime.unsafeFromString("2023-08")
+    val date: FieldTerm.Created =
+      FieldTerm.Created(Comparison.Is, Nel.of(DateTimeRef(pd1), DateTimeRef(pd2)))
+    assertEquals(
+      createdEncoder.encode(ctx, date),
+      SolrQuery(
+        List(
+          SolrToken.fieldIs(
+            Field.Created,
+            SolrToken.fromDateRange(pd1.instantMin(utc), pd1.instantMax(utc))
+          ),
+          SolrToken.fieldIs(
+            Field.Created,
+            SolrToken.fromDateRange(pd2.instantMin(utc), pd2.instantMax(utc))
+          )
+        ).foldOr
+      )
+    )

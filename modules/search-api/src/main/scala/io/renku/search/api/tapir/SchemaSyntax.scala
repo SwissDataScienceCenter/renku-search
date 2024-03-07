2598aacb961941a784e8fd729a740f42a6d45d78
Eike Kettner <eike.kettner@posteo.de>
2024-03-07 15:14:10 +0100

    Use schema syntax for examples

--- modules/search-api/src/main/scala/io/renku/search/api/tapir/SchemaSyntax.scala
+++ modules/search-api/src/main/scala/io/renku/search/api/tapir/SchemaSyntax.scala
@@ -25,7 +25,7 @@ import sttp.tapir.Schema
 trait SchemaSyntax:
 
   extension [T](self: Schema[T])
-    def jsonExample(value: T)(using Encoder[T]): Schema[T] =
+    def jsonExample[TT >: T](value: TT)(using Encoder[TT]): Schema[T] =
       self.encodedExample(Json.encode(value).toUtf8String)
 
 object SchemaSyntax extends SchemaSyntax

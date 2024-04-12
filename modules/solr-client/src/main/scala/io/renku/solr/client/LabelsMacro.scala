a3b85de16a45187e00d88620a0b4ee9a153fe8e4
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2024-04-12 12:10:37 +0200

    Support for borers key annotation in EncoderSupport derivations

---
+++ modules/solr-client/src/main/scala/io/renku/solr/client/LabelsMacro.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.solr.client
+
+import scala.quoted.{Expr, Quotes, Type, Varargs}
+import io.bullet.borer.derivation.key
+
+private object LabelsMacro:
+  inline def findLabels[T]: Seq[String] = ${ findLabelsImpl[T] }
+
+  private def findLabelsImpl[T: Type](using q: Quotes): Expr[Seq[String]] = {
+    import q.reflect.*
+
+    val keyAnno = TypeRepr.of[io.bullet.borer.derivation.key]
+
+    def keyAnnotation(s: Symbol): Expr[Option[String]] = {
+      val key = s.annotations.find(_.tpe <:< keyAnno).map {
+        case Apply(_, List(Literal(StringConstant(x)))) => x
+        case e =>
+          report.errorAndAbort("Only strings supported for 'key' annotation", e.pos)
+      }
+      Expr(key)
+    }
+
+    val sym = TypeRepr.of[T].typeSymbol
+    val caseParams = sym.caseFields
+    val fields = Varargs(caseParams.map(sym => Expr(sym.name)))
+    val fieldAnns = Varargs(caseParams.map(keyAnnotation))
+    '{
+      $fields.zip($fieldAnns).map(t => t._2.getOrElse(t._1))
+    }
+  }
--- modules/solr-client/src/test/scala/io/renku/solr/client/JsonEncodingSpec.scala
+++ modules/solr-client/src/test/scala/io/renku/solr/client/JsonEncodingSpec.scala
@@ -20,8 +20,10 @@ package io.renku.solr.client
 
 import io.bullet.borer._
 import io.bullet.borer.derivation.MapBasedCodecs.deriveDecoder
-import io.renku.solr.client.JsonEncodingSpec.Room
+import io.renku.solr.client.JsonEncodingSpec.{Animal, Room}
 import munit.FunSuite
+import io.bullet.borer.derivation.key
+import io.bullet.borer.derivation.MapBasedCodecs
 
 class JsonEncodingSpec extends FunSuite {
 
@@ -31,6 +33,21 @@ class JsonEncodingSpec extends FunSuite {
     val rr = Json.decode(json.getBytes).to[Room].value
     assertEquals(json, """{"_type":"Room","name":"meeting room","seats":59}""")
     assertEquals(rr, r)
+
+  test("encode with key annotation"):
+    val c = JsonEncodingSpec.Course("my course", 25)
+    val json = Json.encode(c).toUtf8String
+    assertEquals(json, """{"type":"Course","name":"my course","price":25}""")
+
+  test("encode sealed hierarchies"):
+    val dog: Animal = Animal.Dog("bello")
+    val cat: Animal = Animal.Cat("maunzi")
+    val dogJson = Json.encode(dog).toUtf8String
+    val catJson = Json.encode(cat).toUtf8String
+    val cat0 = Json.decode(catJson.getBytes).to[Animal].value
+    assertEquals(cat0, cat)
+    val dog0 = Json.decode(dogJson.getBytes).to[Animal].value
+    assertEquals(dog0, dog)
 }
 
 object JsonEncodingSpec:
@@ -38,3 +55,22 @@ object JsonEncodingSpec:
   object Room:
     given Decoder[Room] = deriveDecoder
     given Encoder[Room] = EncoderSupport.deriveWithDiscriminator[Room]("_type")
+
+  case class Course(name: String, @key("price") dollars: Int)
+  object Course:
+    given Encoder[Course] = EncoderSupport.deriveWithDiscriminator[Course]("type")
+
+  sealed trait Animal
+  object Animal:
+    given AdtEncodingStrategy =
+      AdtEncodingStrategy.flat(typeMemberName = "type")
+    given Decoder[Animal] = MapBasedCodecs.deriveAllDecoders[Animal]
+    given Encoder[Animal] = EncoderSupport.derive[Animal]
+
+    final case class Dog(name: String) extends Animal
+    object Dog:
+      given Encoder[Dog] = EncoderSupport.deriveWithDiscriminator[Dog]("type")
+
+    final case class Cat(name: String) extends Animal
+    object Cat:
+      given Encoder[Cat] = EncoderSupport.deriveWithDiscriminator[Cat]("type")

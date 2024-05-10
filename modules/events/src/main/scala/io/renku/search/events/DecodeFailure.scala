1c8844a4495aaa7dcaa042cf22623313ba1841d1
eikek <701128+eikek@users.noreply.github.com>
2024-05-10 10:32:16 +0200

    fix: Support json and binary avro values (#114)
    - decode message header trying both formats
    - message payloads are then decoded using the advertised format in the header

--- modules/events/src/main/scala/io/renku/search/events/DecodeFailure.scala
+++ modules/events/src/main/scala/io/renku/search/events/DecodeFailure.scala
@@ -18,6 +18,11 @@
 
 package io.renku.search.events
 
+import cats.data.NonEmptyList
+
+import io.renku.events.Header
+import scodec.bits.ByteVector
+
 trait DecodeFailure extends RuntimeException
 
 object DecodeFailure:
@@ -33,4 +38,29 @@ object DecodeFailure:
   final case class VersionNotSupported(id: MessageId, header: MessageHeader)
       extends NoStackTrace(
         s"Version ${header.schemaVersion} not supported for payload in message $id (header: $header)"
+      )
+
+  final case class HeaderReadError(
+      data: ByteVector,
+      causeBinary: Throwable,
+      causeJson: Throwable
+  ) extends NoStackTrace(
+        s"Reading message header failed! Binary-Error: ${causeBinary.getMessage} Json-Error: ${causeJson.getMessage}"
+      ):
+    addSuppressed(causeBinary)
+    addSuppressed(causeJson)
+
+  final case class NoHeaderRecord(data: ByteVector)
+      extends NoStackTrace(
+        s"No header record found in byte vector: $data"
+      )
+
+  final case class MultipleHeaderRecords(data: ByteVector, headers: NonEmptyList[Header])
+      extends NoStackTrace(
+        s"Multiple header records (${headers.size}) found. Required exactly one."
+      )
+
+  final case class FieldReadError(fieldName: String, value: String, message: String)
+      extends NoStackTrace(
+        s"Reading field '$fieldName' with value '$value' failed: $message"
       )
---
+++ modules/events/src/test/scala/io/renku/search/events/MessageHeaderSpec.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2024 Swiss Data Science Center (SDSC)
+ * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+ * Eidgenössische Technische Hochschule Zürich (ETHZ).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.renku.search.events
+
+import java.time.Instant
+
+import io.renku.avro.codec.AvroEncoder
+import io.renku.avro.codec.AvroWriter
+import io.renku.avro.codec.all.given
+import io.renku.events.Header as HeaderV2
+import io.renku.events.v1.Header as HeaderV1
+import io.renku.search.model.Timestamp
+import munit.*
+import scodec.bits.ByteVector
+
+class MessageHeaderSpec extends FunSuite:
+
+  DataContentType.values.foreach { ct =>
+    test(s"read v1 header: $ct"):
+      val now = Instant.now
+      val hv1 = HeaderV1("the-source", "type2", ct.mimeType, "v1", now, "req1")
+      val bv1 = AvroWriter(HeaderV1.SCHEMA$).writeData(ct, Seq(hv1))
+      val h = MessageHeader.fromByteVector(bv1).fold(throw _, identity)
+      assertEquals(
+        h,
+        MessageHeader(
+          MessageSource("the-source"),
+          ct,
+          SchemaVersion.V1,
+          Timestamp(now),
+          RequestId("req1")
+        )
+      )
+  }
+
+  DataContentType.values.foreach { ct =>
+    test(s"read v2 header: $ct"):
+      val now = Instant.now
+      val hv2 = HeaderV2("the-source", "type2", ct.mimeType, "v1", now, "req1")
+      val bv1 = AvroWriter(HeaderV2.SCHEMA$).writeData(ct, Seq(hv2))
+      val h = MessageHeader.fromByteVector(bv1).fold(throw _, identity)
+      assertEquals(
+        h,
+        MessageHeader(
+          MessageSource("the-source"),
+          ct,
+          SchemaVersion.V1,
+          Timestamp(now),
+          RequestId("req1")
+        )
+      )
+  }
+
+  extension (self: AvroWriter)
+    def writeData[A: AvroEncoder](ct: DataContentType, values: Seq[A]): ByteVector =
+      ct match
+        case DataContentType.Binary => self.write[A](values)
+        case DataContentType.Json   => self.writeJson[A](values)

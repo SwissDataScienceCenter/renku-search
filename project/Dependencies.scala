890708c808808d0bee661da70602262a8507f0bf
RenkuBot <RenkuBot@users.noreply.github.com>
2024-05-11 06:02:23 +0000

    chore: Update openapi-circe-yaml from 0.9.0 to 0.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val scodec = "2.2.2"
     val scodecBits = "1.1.38"
     val scribe = "3.13.5"
-    val sttpApiSpec = "0.9.0"
+    val sttpApiSpec = "0.10.0"
     val tapir = "1.10.5"
     val jwtScala = "10.0.0";
   }

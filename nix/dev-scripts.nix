6d36c439e72bc0fa7c892963af06f59e7f4e6058
eikek <701128+eikek@users.noreply.github.com>
2024-05-22 09:11:25 +0200

    Remove obsolete scripts and update flake (#134)
    - Bash script to issue renku events are now obsolete, since
      `search-cli` now covers them all
    - Update nix flake

--- nix/dev-scripts.nix
+++ nix/dev-scripts.nix
@@ -3,41 +3,6 @@
   writeShellScriptBin,
   devshell-tools,
 }: rec {
-  redis-project-create = devshell-tools.lib.installScript {
-    script = ./scripts/redis-project-create;
-    inherit system;
-  };
-
-  redis-project-update = devshell-tools.lib.installScript {
-    script = ./scripts/redis-project-update;
-    inherit system;
-  };
-
-  redis-project-remove = devshell-tools.lib.installScript {
-    script = ./scripts/redis-project-remove;
-    inherit system;
-  };
-
-  redis-user-add = devshell-tools.lib.installScript {
-    script = ./scripts/redis-user-add;
-    inherit system;
-  };
-
-  redis-user-remove = devshell-tools.lib.installScript {
-    script = ./scripts/redis-user-remove;
-    inherit system;
-  };
-
-  redis-auth-add = devshell-tools.lib.installScript {
-    script = ./scripts/redis-auth-add;
-    inherit system;
-  };
-
-  redis-auth-remove = devshell-tools.lib.installScript {
-    script = ./scripts/redis-auth-remove;
-    inherit system;
-  };
-
   redis-port-forward = devshell-tools.lib.installScript {
     script = ./scripts/redis-port-forward;
     inherit system;

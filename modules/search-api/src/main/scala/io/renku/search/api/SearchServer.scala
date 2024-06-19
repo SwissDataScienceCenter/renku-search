82502888e976d73dc18f56428bec7ab5cbe68c86
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2024-06-19 11:49:29 +0200

    Don't log operation routes (ping) and openapi spec

--- modules/search-api/src/main/scala/io/renku/search/api/SearchServer.scala
+++ modules/search-api/src/main/scala/io/renku/search/api/SearchServer.scala
@@ -27,7 +27,6 @@ import io.renku.search.http.HttpServer
 import io.renku.search.http.metrics.MetricsRoutes
 import io.renku.search.http.routes.OperationRoutes
 import io.renku.search.metrics.CollectorRegistryBuilder
-import org.http4s.HttpApp
 import org.http4s.HttpRoutes
 import org.http4s.server.middleware.ResponseLogger
 import org.http4s.server.middleware.{RequestId, RequestLogger}
@@ -37,10 +36,9 @@ object SearchServer:
   val pathPrefix = List("api", "search")
   def create[F[_]: Async: Network](config: SearchApiConfig, app: ServiceRoutes[F]) =
     for
-      logger <- Resource.pure(scribe.cats.effect[F])
       routes <- makeHttpRoutes(app)
       server <- HttpServer[F](config.httpServerConfig)
-        .withHttpApp(withMiddleware(logger, routes.orNotFound))
+        .withHttpApp(routes.orNotFound)
         .build
     yield server
 
@@ -52,22 +50,31 @@ object SearchServer:
     val opRoutes = OperationRoutes[F]
     val metricRoutes = MetricsRoutes[F](CollectorRegistryBuilder[F].withJVMMetrics)
     for
+      logger <- Resource.pure(scribe.cats.effect[F])
       businessRoutes <- metricRoutes.makeRoutes(app.routes)
-      routes = openApiRoute <+> openApiLegacy <+> businessRoutes <+> opRoutes
+      routes = List(
+        openApiRoute,
+        openApiLegacy,
+        withMiddleware(logger, businessRoutes),
+        opRoutes
+      ).reduce(_ <+> _)
     yield routes
 
-  def withMiddleware[F[_]: Async](logger: Scribe[F], httpApp: HttpApp[F]): HttpApp[F] =
-    middleWares[F](logger).foldLeft(httpApp)((app, mf) => mf(app))
+  def withMiddleware[F[_]: Async](
+      logger: Scribe[F],
+      httpRoutes: HttpRoutes[F]
+  ): HttpRoutes[F] =
+    middleWares[F](logger).foldLeft(httpRoutes)((r, mf) => mf(r))
 
   private def middleWares[F[_]: Async](
       logger: Scribe[F]
-  ): List[HttpApp[F] => HttpApp[F]] =
+  ): List[HttpRoutes[F] => HttpRoutes[F]] =
     val log = (str: String) => logger.info(str)
     List(
       RequestLogger
-        .httpApp(logHeaders = true, logBody = false, logAction = log.some),
-      RequestId.httpApp[F],
-      ResponseLogger.httpApp(
+        .httpRoutes(logHeaders = true, logBody = false, logAction = log.some),
+      RequestId.httpRoutes[F],
+      ResponseLogger.httpRoutes(
         logHeaders = true,
         logBody = false,
         logAction = log.some

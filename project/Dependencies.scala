6ace0d373c5851cbd1131bf7d67401755e3dd9ce
RenkuBot <RenkuBot@users.noreply.github.com>
2024-09-04 06:01:34 +0000

    chore: Update tapir-openapi-docs from 1.11.1 to 1.11.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val scodecBits = "1.2.1"
     val scribe = "3.15.0"
     val sttpApiSpec = "0.11.3"
-    val tapir = "1.11.1"
+    val tapir = "1.11.2"
     val jwtScala = "10.0.1";
   }
 

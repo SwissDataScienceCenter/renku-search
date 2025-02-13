5cea142ec36b4085eb8c777d83db753060e133ea
RenkuBot <RenkuBot@users.noreply.github.com>
2025-02-13 06:03:15 +0000

    chore: Update sentry from 8.1.0 to 8.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     val sttpApiSpec = "0.11.7"
     val tapir = "1.11.14"
     val jwtScala = "10.0.4"
-    val sentry = "8.1.0"
+    val sentry = "8.2.0"
   }
 
   val sentry = Seq(
